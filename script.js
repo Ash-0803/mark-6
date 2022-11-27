9e3eabd9e2b840504d714a297f43845076674473
Ashish Chaudhary <ashish080303@gmail.com>
2022-11-27 08:43:35 +0530

    prime and leap page added and banana css completed

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 btn = document.querySelector('#btn-translate');
 input = document.querySelector('#textarea');
-output = document.querySelector('.output')
+output = document.querySelector('.output');
+navLink = document.querySelectorAll('.nav-link');
+
 btn.addEventListener('click', clickHandler);
+
+navLink.forEach((linkItem, index) => {
+  linkItem.addEventListener('click', ()=>{
+  document.querySelector(linkItem).classList.remove('active');
+  navLink.classList.add('active');
+  console.log( index, linkItem, "clicked")
+  })
+})
 
 function clickHandler() {
   console.log("clicked");
