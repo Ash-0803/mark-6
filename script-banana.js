6afdb3f52ff18094ae55fd68f3035754b51e3f60
Ashish Chaudhary <ashish080303@gmail.com>
2022-11-29 16:19:41 +0530

    all pages done

--- script-banana.js
+++ script-banana.js
@@ -6,6 +6,16 @@ output = document.querySelector('.output');
 btn.addEventListener('click', clickHandler);
 
 function clickHandler() {
+  url = `https://api.funtranslations.com/translate/minion.json?text=${input.value}`;
   console.log("clicked");
-  output.innerText = "lasjdfkljdsfl"+input.value;
+  console.log(input.value)
+  
+  fetch(url)
+  .then(response => response.json())
+  .then(json =>{
+    console.log(json);
+    console.log(url)
+    output.innerText = json.contents.translated;
+  })
+
 }
